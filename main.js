b54955b7af8e054d528faf31a6d0510688441b5c
mahwang <64502474+mahwang@users.noreply.github.com>
2020-07-27 15:34:37 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,218 @@
+Vue.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template: `
+        <div class="product">
+        
+        <div class="product-image">
+            <img v-bind:src="image" alt="Mental health matters sweatshirt"> 
+            <p>{{creditSource}}</p> 
+            <a>https://selfcareisforeveryone.com/</a>
+        </div>
+
+        <div class="product-info">
+            <h1>{{ title }}</h1>
+            <p>{{ price }}</p>
+            <p v-if="inStock">In Stock</p>
+            <p v-else>Out of Stock!</p>
+            <p>Shipping: {{ shipping }}</p>
+
+            <p><u>{{ productDetails }}</u></p>
+        <ul>
+            <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+
+        <p><strong>{{ chooseColor }}</strong></p>
+        <div class="color-box"
+            v-for="(variant, index) in variants"
+            :key="variant.variantId"
+            :style="{ backgroundColor: variant.variantColor }"
+            @mouseover="updateProduct(index)"
+            >
+        </div>
+
+        <button v-on:click="addToCart" 
+            :disabled="!inStock"
+            :class="{ disabledButton: !inStock}"
+            >Add to Cart
+        </button>
+
+        <button @click="removeFromCart"
+            >
+        Remove from cart
+        </button>
+
+    </div>
+
+    <div>
+        <h2>Reviews</h2>
+        <p v-if="!reviews.length">There are no reviews yet.</p>
+        <ul>
+            <li v-for="review in reviews">
+            <p>{{ review.name }}</p>
+            <p>Rating: {{ review.rating }}</p>
+            <p>{{ review.review }}</p>
+            </li>
+        </ul>
+    </div>
+    
+    <product-review @review-submitted="addReview"></product-review>
+
+</div>
+    `,
+    data: function() {
+        return {
+            brand: 'Self Care',
+            product: 'Mental Health Sweatshirt',
+            price: '$35.00',
+            selectedVariant: 0,
+            // image: './mental-health-matters-purple.jpg',
+            productDetails: 'Details: ',
+            chooseColor: 'Select color',
+            details: ["80% cotton", "20% polytester", "Gender neutral"],
+            creditSource: "Interested in more products? Copy and paste this link into your browser: ",
+    
+            variants: [
+                {
+                    variantId: 3945,
+                    variantColor: "purple",
+                    variantImage: './mental-health-matters-purple.jpg',
+                    variantQuantity: 10
+                },
+                {
+                    variantId: 3946,
+                    variantColor: "white",
+                    variantImage: './mental-health-matters-white.jpg',
+                    variantQuantity: 0
+                }
+            ],
+            reviews: []
+        }
+    },
+    methods: {
+        addToCart: function () {
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId)
+        },
+        updateProduct: function(index){
+            this.selectedVariant = index 
+            console.log(index)
+        },
+        removeFromCart: function() {
+            this.$emit('remove-from-cart', this.variants[this.selectedVariant].variantId)
+        },
+        addReview(productReview) {
+            this.reviews.push(productReview)
+        }
+},
+    computed: {
+        title () {
+            return this.brand + ' ' + this.product 
+            // computed result is (cached) saved until dependency is changed 
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage 
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        shipping() {
+            if (this.premium) {
+              return "Free"
+            }
+              return 3.99
+          }
+      },
+  })
+
+  Vue.component('product-review', {
+    template: `
+    <form class="review-form" @submit.prevent="onSubmit">
+
+    <p v-if="errors.length">
+        <b>Please correct the following error(s):</b>
+        <ul>
+            <li v-for="error in errors">{{ error }}</li>
+        </ul>
+    </p>
+
+    <p>
+        <label for="name">Name:</label>
+        <input id="name" v-model="name" placeholder="name">
+    </p>
+        
+    <p>
+        <label for="review">Review:</label>      
+        <textarea id="review" v-model="review"></textarea>
+    </p>
+        
+    <p>
+        <label for="rating">Rating:</label>
+        <select id="rating" v-model.number="rating">
+            <option>5</option>
+            <option>4</option>
+            <option>3</option>
+            <option>2</option>
+            <option>1</option>
+        </select>
+    </p>
+            
+    <p>
+        <input type="submit" value="Submit">  
+    </p>    
+    
+    </form>
+
+    `,
+    data: function() {
+        return {
+            name: null,
+            review: null,
+            rating: null,
+            errors: [],
+        }
+    },
+    methods: {
+        onSubmit() {
+            if (this.name && this.review && this.rating){
+                let productReview = {
+                    name: this.name,
+                    review: this.review, 
+                    rating: this.rating
+                }
+                this.$emit('review-submitted', productReview)
+                this.name = null
+                this.review = null
+                this.rating = null 
+            }
+            else {
+                if(!this.name) this.errors.push("Name required.")
+                if(!this.review) this.errors.push("Review required.")
+                if(!this.rating) this.errors.push("Rating required.")
+            }
+        }
+    }
+})
+
+var app = new Vue({
+    el: '#app',
+    data: {
+      premium: true,
+      cart: []
+    },
+    methods: {
+      updateCart(id) {
+        this.cart.push(id)
+      },
+      removeItem(id) {
+        for(var i = this.cart.length - 1; i >= 0; i--) {
+          if (this.cart[i] === id) {
+             this.cart.splice(i, 1);
+          }
+        }
+      }
+    }
+})
